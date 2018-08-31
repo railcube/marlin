51cc81a62fa1b2cd3b06ed395719d72f24864f63
Vlad Babii <vladbabii@users.noreply.github.com>
2018-09-01 02:39:10 +0300

    Add files via upload

--- 1.1.9/Configuration.h
+++ 1.1.9/Configuration.h
@@ -598,7 +598,7 @@
    Override with M92
                                         X, Y, Z, E0 [, E1[, E2[, E3[, E4]]]]
 */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 161, 161, 800, 200 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 160, 160, 80, 200 }
 
 /**
    Default Max Feed Rate (mm/s)
@@ -613,7 +613,7 @@
    Override with M201
                                         X, Y, Z, E0 [, E1[, E2[, E3[, E4]]]]
 */
-#define DEFAULT_MAX_ACCELERATION       { 3000, 3000, 100, 10000 }
+#define DEFAULT_MAX_ACCELERATION       { 1000, 1000, 100, 10000 }
 
 /**
    Default Acceleration (change/s) change = mm/s
@@ -623,9 +623,9 @@
      M204 R    Retract Acceleration
      M204 T    Travel Acceleration
 */
-#define DEFAULT_ACCELERATION          3000      // X, Y, Z and E acceleration for printing moves
-#define DEFAULT_RETRACT_ACCELERATION  3000    // E acceleration for retracts
-#define DEFAULT_TRAVEL_ACCELERATION   3000    // X, Y, Z acceleration for travel (non printing) moves
+#define DEFAULT_ACCELERATION           600      // X, Y, Z and E acceleration for printing moves
+#define DEFAULT_RETRACT_ACCELERATION  1000    // E acceleration for retracts
+#define DEFAULT_TRAVEL_ACCELERATION   1000    // X, Y, Z acceleration for travel (non printing) moves
 
 /**
    Default Jerk (mm/s)
@@ -635,8 +635,8 @@
    When changing speed and direction, if the difference is less than the
    value set here, it may happen instantaneously.
 */
-#define DEFAULT_XJERK                 10.0
-#define DEFAULT_YJERK                 10.0
+#define DEFAULT_XJERK                 7.0
+#define DEFAULT_YJERK                 7.0
 #define DEFAULT_ZJERK                  0.3
 #define DEFAULT_EJERK                  5.0
 
@@ -869,8 +869,8 @@
 // @section machine
 
 // The size of the print bed
-#define X_BED_SIZE 220
-#define Y_BED_SIZE 220
+#define X_BED_SIZE 320
+#define Y_BED_SIZE 420
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
 #define X_MIN_POS 0
