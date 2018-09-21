54dfea4789e04c715dab7410f732d86b5cbd617d
Vlad Babii <vladbabii@users.noreply.github.com>
2018-09-21 20:02:26 +0300

    Add files via upload

--- 1.1.9/Configuration.h
+++ 1.1.9/Configuration.h
@@ -598,7 +598,7 @@
    Override with M92
                                         X, Y, Z, E0 [, E1[, E2[, E3[, E4]]]]
 */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 160, 160, 80, 200 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 160, 160, 800, 200 }
 
 /**
    Default Max Feed Rate (mm/s)
